83c5d40898646a63935353e2808ba0c1a1da8025
Matěj Kříž <lyzynec@protonmail.com>
2024-10-24 13:49:44 +0200

    Adding general data definitions.

---
+++ src/general.h
@@ -0,0 +1,10 @@
+#ifndef __PSOI_GENERAL_H__
+#define __PSOI_GENERAL_H__
+
+// PSOI_FLOAT is the floating point type used in the PSOI algorithm.
+// It can be defined as `float` or `double`.
+#ifndef PSOI_FLOAT
+#define PSOI_FLOAT double
+#endif
+
+#endif // __PSOI_GENERAL_H__
